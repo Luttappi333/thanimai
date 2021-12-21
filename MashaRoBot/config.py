e91a5be6bc5cdd727931442fb3d84ee7bbdfa99e
KIRANRAJBOT <88644594+KIRANRAJBOT@users.noreply.github.com>
2021-12-21 22:04:17 +0530

    Update config.py

--- MashaRoBot/config.py
+++ MashaRoBot/config.py
@@ -15,9 +15,9 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "owo"
-    TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    API_ID = 8962397  # integer value, dont use ""
+    API_HASH = "0336e890a385149db77821a136a7fce3"
+    TOKEN = "2052309535:AAGo7jA9zJBQoyHyadbdUYRymBjveaKY1M0"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1732814103  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Horimaya"
     SUPPORT_CHAT = 'thanimaisupport'  #Your own group for support, do not add the @
