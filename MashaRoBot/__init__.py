b49b99727fd289702e1a797eeb5389497c95f945
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-22 00:16:02 +0530

    Update __init__.py

--- MashaRoBot/__init__.py
+++ MashaRoBot/__init__.py
@@ -110,7 +110,7 @@ if ENV:
     SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
     SPAMWATCH_API = os.environ.get('SPAMWATCH_API', None)
-    BOT_ID = 1642345892
+    BOT_ID = "2052309535"
 
 
     LOG_GROUP_ID = -1001498089395
