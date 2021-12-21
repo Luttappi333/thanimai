08469d58268bd4726e8edc837cc71dfd14c71990
KIRANRAJBOT <88644594+KIRANRAJBOT@users.noreply.github.com>
2021-12-21 22:03:44 +0530

    Update config.py

--- MashaRoBot/config.py
+++ MashaRoBot/config.py
@@ -20,7 +20,7 @@ class Config(object):
     TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1732814103  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Horimaya"
-    SUPPORT_CHAT = 'RemiSupport'  #Your own group for support, do not add the @
+    SUPPORT_CHAT = 'thanimaisupport'  #Your own group for support, do not add the @
     JOIN_LOGGER = -1001739802989  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -1001739802989  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
